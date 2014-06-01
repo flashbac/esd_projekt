12d8c1e0d4bba850a8e0dc2cabb33ba6e5f16947
René Galow <rensky.g@googlemail.com>
2014-06-01 18:52:05 +0200

    merge

--- server/threadedServer/src/Kommunikation/TcpProtokoll.h
+++ server/threadedServer/src/Kommunikation/TcpProtokoll.h
@@ -8,8 +8,10 @@
 #ifndef KOMMUNIKATIONSPROTOKOLL_H_
 #define KOMMUNIKATIONSPROTOKOLL_H_
 
-#include <jsoncpp/json/value.h>
-#include <jsoncpp/json/reader.h>
+//#include <jsoncpp/json/value.h>
+//#include <jsoncpp/json/reader.h>
+#include <jsoncpp/value.h>
+#include <jsoncpp/reader.h>
 #include "TcpConnection.h"
 #include "KommunikationTypes.h"
 
