8ffa3eaf5933d0ee5ac95ad266b59f786ec891b4
René Galow <rensky@gmx.de>
2014-06-03 00:21:19 +0200

    anderungen für cameraeinstellungen

--- server/threadedServer/src/main.cpp
+++ server/threadedServer/src/main.cpp
@@ -18,11 +18,11 @@ int main(int argc, char** argv) {
 
 	sem_t sem_print;
 	//std::string ip = "141.64.166.22";
-	//std::string ip = "192.168.1.126";
-	std::string ip = "192.168.178.42";
+	std::string ip = "192.168.1.230";
+	//std::string ip = "192.168.178.42";
 	std::string device = "eth0";
 	int port = 50000;
-	int camID = 0;
+	int camID = 1;
 	int MTU = 1500;
 	double cameraWidth = 640.0;
 	double cameraHeigth = 480.0;
@@ -34,13 +34,13 @@ int main(int argc, char** argv) {
 	}
 
 
-	TcpListenner tcpL;
-	tcpL.start();
-
-	while (1)
-	{
-	usleep(20);
-	}
+//	TcpListenner tcpL;
+//	tcpL.start();
+//
+//	while (1)
+//	{
+//	usleep(20);
+//	}
 
 	//argv auswertung
 	// ip
